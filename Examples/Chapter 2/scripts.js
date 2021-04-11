9feb645affe27dc62f281ceb2a20f029487438e5
Cody Yang <codyyang@Codys-Mac.local>
2021-04-11 07:21:49 +0100

    d3.json file changed to .then style.

--- Examples/Chapter 2/scripts.js
+++ Examples/Chapter 2/scripts.js
@@ -3,7 +3,24 @@ function load_csv() {
     d3.csv("./cities.csv", data => console.log(data));
 }
 
+var json_data = 0;
+
 function load_json() {
-    d3.json("./tweets.json", data => console.log(data));
+    d3.json("./tweets.json").then(function (data) {
+        var tweetData = data.tweets;
+        //var nestedTweets = d3.nest().key(d => d.user).entries(tweetData);
+        console.log(data.tweets);
+        //console.log(nestedTweets);
+    });
 }
 
+d3.json("users.json").then(function (data) {
+    var tweetData = data;
+    // var nestedTweets = d3.nest().key(d => d.user).entries(tweetData);
+    console.log(data);
+    // console.log(nestedTweets)
+});
+
+// d3.csv("./cities.csv", data => console.log(data));
+
+// load_csv();
